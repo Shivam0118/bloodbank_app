2877f1ed6283d5e686d54332aadcf50d46387fb2
Shivam0118 <shivam8728949475@gmail.com>
2023-01-21 18:38:11 +0530

    conatiner created

---
+++ lib/blood_counter.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BloodCounterScreen extends StatelessWidget {
+  const BloodCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Row(
+            children: [
+              Container(
+                  height: 150,
+                  width: MediaQuery.of(context).size.width * 0.48,
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade600,
+                      borderRadius: BorderRadius.all(Radius.circular(10))))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
