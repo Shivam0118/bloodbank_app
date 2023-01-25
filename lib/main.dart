fcca83dcf58ea03a8aba51861f5bea185472f283
Shivam0118 <shivam8728949475@gmail.com>
2023-01-25 21:33:56 +0530

    created otp screen and other minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
-import 'package:bloodbank_app/mobile_number.dart';
+// ignore_for_file: unused_import, prefer_const_constructors
+
+import 'package:bloodbank_app/constants/routes.dart';
+import 'package:bloodbank_app/screens/all_messages.dart';
+import 'package:bloodbank_app/screens/donors_map.dart';
+import 'package:bloodbank_app/screens/find_donors.dart';
+import 'package:bloodbank_app/screens/history.dart';
+import 'package:bloodbank_app/screens/home.dart';
+import 'package:bloodbank_app/screens/incoming_requests.dart';
+import 'package:bloodbank_app/screens/messages.dart';
+import 'package:bloodbank_app/screens/mobile_number.dart';
+import 'package:bloodbank_app/screens/onboarding/onboarding0.dart';
+import 'package:bloodbank_app/screens/onboarding/onboarding1.dart';
+import 'package:bloodbank_app/screens/onboarding/onboarding2.dart';
+import 'package:bloodbank_app/screens/otp_screen.dart';
+import 'package:bloodbank_app/screens/sign_up_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'package:bloodbank_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +25,40 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Blood Bank App",
-      home: MobileNumber(),
+      title: 'Bllood Bank App',
+      theme: ThemeData(
+          primarySwatch: Colors.red,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+                  // backgroundColor: MaterialStatePropertyAll(Color(0xA8FF0E0E))))
+                  backgroundColor: MaterialStateColor.resolveWith(
+            // ((states) => Color.fromARGB(168.3, 255, 14, 14)),
+            ((states) => Color(0xa8ff0e0e)),
+            // 255*0.6
+          )))
+          // buttonTheme:
+          ),
+      // home: SplashScreen(),
+      initialRoute: Routes.otpScreen,
+      routes: {
+        "/": (context) => SplashScreen(),
+        Routes.onboardingScreen: (context) => OnboardingPage(),
+        Routes.onboarding1Screen: (context) => OnboardingPage1(),
+        Routes.onboarding2Screen: (context) => OnboardingPage2(),
+        Routes.loginScreen: (context) => LoginScreen(),
+        Routes.otpScreen: (context) => OtpScreen(),
+        Routes.signUpScreen: (context) => SignUpScreen(),
+        Routes.home: (context) => Home(),
+        Routes.findDonors: (context) => FindDonors(),
+        Routes.incomingRequests: (context) => IncomingRequests(),
+        Routes.history: (context) => History(),
+        Routes.allMessages: (context) => AllMessages(),
+        Routes.donorsMap: (context) => DonorsMap(),
+        Routes.messages: (context) => Messages(),
+      },
     );
   }
 }
